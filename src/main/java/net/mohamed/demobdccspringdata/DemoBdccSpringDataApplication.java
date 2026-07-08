0c6afa663c9410e6bbe9b0b830d9c3b57f3c0bfd
Mochatr <mohamedchatr8@gmail.com>
2026-07-08 01:23:30 +0100

    Initial commit: project structure

---
+++ src/main/java/net/mohamed/demobdccspringdata/DemoBdccSpringDataApplication.java
@@ -0,0 +1,13 @@
+package net.mohamed.demobdccspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBdccSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBdccSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mohamed/demobdccspringdata/DemoBdccSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mohamed.demobdccspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBdccSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
