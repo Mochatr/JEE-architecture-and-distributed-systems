1dd5fba627a0d0d0ca8971f9e60200fe23ebde22
Mochatr <mohamedchatr8@gmail.com>
2026-07-09 00:20:25 +0100

    Ajouter un produit

--- src/main/java/net/mohamed/demobdccspringdata/web/ProductRestController.java
+++ src/main/java/net/mohamed/demobdccspringdata/web/ProductRestController.java
@@ -3,9 +3,7 @@ package net.mohamed.demobdccspringdata.web;
 import net.mohamed.demobdccspringdata.entities.Product;
 import net.mohamed.demobdccspringdata.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,10 @@ public class ProductRestController {
     @GetMapping("/find/products")
     public List<Product> findProducts(String kw){
         return productRepository.findByNameContains(kw);
+    }
+
+    @PostMapping("/products")
+    public Product save(@RequestBody Product product){
+        return productRepository.save(product);
     }
 }
